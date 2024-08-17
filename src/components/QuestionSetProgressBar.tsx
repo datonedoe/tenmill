afe96687c8e053dd3d06aa29d934ae1a84ed7250
Darwin Do <darwindo@outlook.com>
2024-08-17 13:22:29 -0400

    make progress bar dynamic to number of questions

--- src/components/QuestionSetProgressBar.tsx
+++ src/components/QuestionSetProgressBar.tsx
@@ -1,13 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { QuestionContext } from '../App';
 
 function QuestionSetProgressBar() {
-  return (
-    <div className="QuestionSetProgressBar my-8">
-        <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
-        <div className="bg-green-600 h-1.5 rounded-full dark:bg-green-500" style={{width: "45%"}}></div>
+    const context  = useContext(QuestionContext);
+    const { state } = context;
+    const { question_index, questions } = state;
+    const [progress, setProgress] = React.useState(0);
+
+    useEffect(() => {
+        setProgress((question_index / questions.length)*100)
+    }, [question_index])
+
+    return (
+        <div className="QuestionSetProgressBar my-8">
+            <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+            <div className="bg-green-600 h-1.5 rounded-full dark:bg-green-500" style={{width: `${progress}%`}}></div>
+            </div>
         </div>
-    </div>
-  );
-}
+    );
+    }
 
 export default QuestionSetProgressBar;
