45a37081e435c930772895caf65738728951ac86
Darwin Do <darwindo@outlook.com>
2024-08-23 15:52:07 -0400

    add question progress comment

---
+++ src/components/QuestionSetProgressComment.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuestionSetProgressComment = () => {
+  return (
+    <div>
+      <div>Wut! 2 in a row!</div>
+    </div>
+  );
+};
+
+export default QuestionSetProgressComment;
